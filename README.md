737c72b98e7a343429bf15f98f6a14eb07b099dc
Atul Bhardwaj <atulonline17@gmail.com>
2022-01-12 21:00:50 +0530

    Program Completed in entirety

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 #Calender
 
-This program shows the Calender of a month according to the date input by the user. 
+This program shows the Calender of a month according to the date input by the user.
+
+##Sample Run
+
+Enter a date: 12/1/2022
+
+Please find the required Calender below
+
+Sun    Mon     Tues    Wed     Thurs    Fri    Sat
+0       0       0       0       0       0       0       
+0       0       0       0       0       0       0       
+0       0       0       12      13      14      15      
+16      17      18      19      20      21      22      
+23      24      25      26      27      28      29      
+30      31      0       0       0       0       0       
--- main.go
+++ main.go
@@ -1,14 +1,148 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func main() {
-	var day, month, year int
-	fmt.Println("Enter the Day: ")
-	fmt.Scanln(&day)
-	fmt.Println("Enter the Month: ")
-	fmt.Scanln(&month)
-	fmt.Println("Enter the year: ")
-	fmt.Scanln(&year)
-	fmt.Println("Entered Date: ", day, "/", month, "/", year)
+
+	var date string
+	fmt.Printf("Enter a date: ")
+	fmt.Scanln(&date)
+
+	str := strings.Split(date, "/")
+
+	day, _ := strconv.Atoi(str[0])
+	month, _ := strconv.Atoi(str[1])
+	year, _ := strconv.Atoi(str[2])
+
+	/*myMap := make(map[string][]int)
+
+	myMap["Sunday"] = []int{}
+	myMap["Monday"] = []int{}
+	myMap["Tuesday"] = []int{}
+	myMap["Wednesday"] = []int{}
+	myMap["Thursday"] = []int{}
+	myMap["Friday"] = []int{}
+	myMap["Saturday"] = []int{} */
+
+	x := time.January
+
+	switch month {
+	case 1:
+		x = time.January
+	case 2:
+		x = time.February
+	case 3:
+		x = time.March
+	case 4:
+		x = time.April
+	case 5:
+		x = time.May
+	case 6:
+		x = time.June
+	case 7:
+		x = time.July
+	case 8:
+		x = time.August
+	case 9:
+		x = time.September
+	case 10:
+		x = time.October
+	case 11:
+		x = time.November
+	case 12:
+		x = time.December
+	}
+
+	firstDayOfMonth := time.Date(year, x, 1, 1, 1, 1, 1, time.UTC)
+	firstDayOfMonthWeekday := firstDayOfMonth.Weekday().String()
+	monthOfFirstDayOfMonth := firstDayOfMonth.Month().String()
+
+	myMap := make(map[string]int)
+
+	myMap["January"] = 31
+	if ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0) {
+		myMap["February"] = 29
+	} else {
+		myMap["February"] = 28
+	}
+	myMap["March"] = 31
+	myMap["April"] = 30
+	myMap["May"] = 31
+	myMap["June"] = 30
+	myMap["July"] = 31
+	myMap["August"] = 31
+	myMap["September"] = 30
+	myMap["October"] = 31
+	myMap["November"] = 30
+	myMap["December"] = 31
+
+	/*newDate := myDate
+	newMonth := month
+	newYear := year
+
+	for newMonth <= month && newYear <= year {
+		myMap[newDate.Weekday().String()] = append(myMap[newDate.Weekday().String()], newDate.Day())
+		newDate = newDate.Add(24 * time.Hour)
+		newMonth = int(newDate.Month())
+		newYear = newDate.Year()
+	}
+
+	for key, value := range myMap {
+		fmt.Println(key, ": ", value)
+	}*/
+
+	newMap := make(map[string]int)
+
+	newMap["Sunday"] = 0
+	newMap["Monday"] = 1
+	newMap["Tuesday"] = 2
+	newMap["Wednesday"] = 3
+	newMap["Thursday"] = 4
+	newMap["Friday"] = 5
+	newMap["Saturday"] = 6
+
+	arr := [6][7]int{}
+
+	lim := myMap[monthOfFirstDayOfMonth]
+	counter := 1
+
+	for j := newMap[firstDayOfMonthWeekday]; j < 7; j += 1 {
+		arr[0][j] = counter
+		counter += 1
+	}
+
+	for i := 1; i < 6 && counter <= lim; i += 1 {
+		for j := 0; j < 7 && counter <= lim; j += 1 {
+			arr[i][j] = counter
+			counter += 1
+		}
+	}
+
+	for i := 0; i < 6; i += 1 {
+		for j := 0; j < 7; j += 1 {
+			if arr[i][j] < day {
+				arr[i][j] = 0
+			}
+		}
+	}
+
+	fmt.Println("\nPlease find the required Calender below\n")
+
+	fmt.Println("Sun    Mon     Tues    Wed     Thurs    Fri    Sat")
+	for i := 0; i < 6; i += 1 {
+		for j := 0; j < 7; j += 1 {
+			if arr[i][j] < 10 {
+				fmt.Printf("%d       ", arr[i][j])
+			} else {
+				fmt.Printf("%d      ", arr[i][j])
+			}
+		}
+		fmt.Println()
+	}
+
 }
